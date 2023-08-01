ed0d242d7801b7cc2fd4661317e4d8f3f8f3ac59
Yuriy <bondarev.bbv@mail.ru>
2023-08-01 13:32:53 +0200

    First Task

---
+++ main.js
@@ -0,0 +1,17 @@
+const numberOfFilms =  +prompt ('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+  'count':  numberOfFilms,
+  'movies': {},
+  'actors': {},
+  'genres': [],
+  'privat': false,
+};
+const a = prompt('один из последних просмотренных фильмов?',''),
+      b = +prompt('на сколько оцениваете его?',''),
+      c = prompt('один из последних просмотренных фильмов?',''),
+      d = +prompt('на сколько оцениваете его?','');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
